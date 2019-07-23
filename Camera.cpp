695b96047f18cc56b1b3dda2b43c9bb86d4605d1
tommypan <709450846@qq.com>
2019-07-23 20:31:06 +0800

    modify camera

--- Camera.cpp
+++ Camera.cpp
@@ -2,8 +2,11 @@
 #include "RenderSetting.h"
 #include "Scene.h"
 #include <list>
+#include <cmath>
 
-Camera::Camera():nearZ(1),farZ(1000)
+#define CLAMP(x, upper, lower) (min(upper, max(x, lower)))
+
+Camera::Camera():nearZ(1),farZ(1000), projection(CameraProjection::Perspective), minFov(1),maxFov(180), fov(30)
 {
 
 }
@@ -14,12 +17,21 @@ Camera::~Camera()
 
 void Camera::Render()
 {
-	XMVECTOR target = XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f);
-	XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	Vector3 target(0.0f, 0.0f, 0.0f);
+	Vector3  up(0.0f, 1.0f, 0.0f);
+	Matrix v= Matrix::CreateLookAt(_transform->GetPosition(), Vector3::Zero, Vector3::Up);
+	//XMStoreFloat4x4(&m_view, v);
+	_transform->SetRotation(Quaternion::CreateFromRotationMatrix(v));
+	XMStoreFloat4x4(&m_view, _transform->GetWorldTransform().Invert());
 
-	XMMATRIX V = XMMatrixLookAtLH((XMVECTOR)_transform->GetPosition(), target, up);
-	XMStoreFloat4x4(&m_view, V);
-	XMMATRIX T = XMMatrixPerspectiveFovLH(XM_PIDIV4, RenderSetting::GetIntance()->m_width / static_cast<float>(RenderSetting::GetIntance()->m_height),
+	//XMVECTOR target = XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f);
+	//XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	//XMMATRIX V = XMMatrixLookAtLH((XMVECTOR)_transform->GetPosition(), target, up);
+	//XMStoreFloat4x4(&m_view, V);
+
+	fov = CLAMP(fov,maxFov,minFov);
+	XMMATRIX T = XMMatrixPerspectiveFovLH(fov/ maxFov*XM_PI, RenderSetting::GetIntance()->m_width / static_cast<float>(RenderSetting::GetIntance()->m_height),
 		nearZ, farZ);
 	XMStoreFloat4x4(&m_proj, T);
 
--- Camera.h
+++ Camera.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "Entity.h"
 
+enum CameraProjection
+{
+	Perspective = 0,
+	Orthongaphics = 1,
+};
 class Camera : public Entity
 {
 public:
@@ -13,6 +18,7 @@ private:
 	void RenderOpacity();
 	void RenderTransparent();
 public:
+	CameraProjection projection;
 	float fov;
 	float nearZ;
 	float farZ;
@@ -21,4 +27,6 @@ public:
 private:
 	Matrix	m_view;
 	Matrix	m_proj;
+	int minFov;
+	int maxFov;
 };
